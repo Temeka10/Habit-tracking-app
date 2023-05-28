4a74f302ed774ac162a1bbcdd6a3afb87b941c3d
Artem Mandych <artemmandych@gmail.com>
2023-05-28 23:07:38 +0300

    Initial Commit

---
+++ Habit-tracking app/Habit_tracking_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_tracking_appApp.swift
+//  Habit-tracking app
+//
+//  Created by Artem Mandych on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Habit_tracking_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
